e9ba756da51a088a624e92814fcf70d27a4dd1f3
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-03-03 17:14:40 +0100

    implementing tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import './App.scss'
 import { useAuthValue } from './context/AuthContext'
-import User from './services/authConnect'
+import Project from './services/projectsConnect'
 
 function App() {
+  const queryClient = useQueryClient()
 
+  const query = useQuery({ queryKey: ['projects'], queryFn: Project.getProjects })
+
+  console.log(query.data)
   return (
     <div className="App">
       <div>
-        <button onClick={login}> Click for login </button>
-      </div>
-       
+        <button> Click for login </button>
+      </div>      
     </div>
   )
 }

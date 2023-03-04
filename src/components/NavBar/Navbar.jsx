c3cecb48c7f4ecb79f48afbefab08724d223ef50
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-03-04 11:13:45 +0100

    nav missing the logo now

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,11 +1,22 @@
+import { NavLink } from 'react-router-dom'
 import './Navbar.scss'
 
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+	return (
+		<nav className='navbar'>
+			<div className='navbar-logo'>
+				<img src='' alt='logo' />
+			</div>
+			<div className='navbar-links'>
+				<NavLink to={'/'}>Home</NavLink>
+				<NavLink to={'/projects'}>Projects</NavLink>
+				<NavLink to={'/about'}>About</NavLink>
+				<NavLink to={'/contact'}>Contact</NavLink>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar

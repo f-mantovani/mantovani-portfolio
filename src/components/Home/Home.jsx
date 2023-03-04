81eb2cd74b6a8a462488ddc28bffeb71cf300a31
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-03-04 21:54:32 +0100

    add typewriter + some global variables to css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,25 @@
 import './Home.scss'
 
-import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<div>
+			<Typewriter
+				options={{
+					strings: [
+						'Web developer',
+						'Software Developer',
+						'Full Stack Developer',
+						'Frontend Developer',
+						'Backend Developer',
+					],
+					autoStart: true,
+					loop: true,
+				}}
+			/>
+		</div>
+	)
 }
 
 export default Home
